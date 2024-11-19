0ba44d065c6939b61843653ab62d5605caf2cde9
PraveenRevvster <praveen@baetho.com>
2024-11-19 18:37:04 +0530

    updated qr code to store contacts

--- client/src/pages/contact-us/ContactUs.js
+++ client/src/pages/contact-us/ContactUs.js
@@ -1,15 +1,28 @@
 import React, { useRef, useState } from 'react'
-import QRCode from "react-qr-code";
-import { toPng, toSvg } from 'html-to-image';
+import { QRCodeSVG } from "qrcode.react";
+import { toPng } from 'html-to-image';
 
 function ContactUs() {
     const [data, setData] = useState('')
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [url, setUrl] = useState('')
 
-    const inputVal = useRef('')
     const qrRef = useRef('')
 
     const generate = () => {
-        setData(inputVal.current.value)
+        const vCard = `BEGIN:VCARD
+VERSION:3.0
+FN:${name}
+TEL:${phone}
+EMAIL:${email}
+URL:${url}
+END:VCARD`;
+
+        setData(vCard)
+        console.log(vCard);
+
     }
 
     const download = async () => {
@@ -25,8 +38,39 @@ function ContactUs() {
     return (
         <div className='w-full h-screen flex flex-col justify-center items-center gap-10'>
             <div>
-                <input ref={inputVal} type='text' placeholder='Type anything'
-                    className='border border-black outline-none rounded px-5 py-2 mr-5' />
+                <div className='flex flex-col gap-4 mb-4'>
+                    {/* <input ref={inputVal} type='text' placeholder='Type anything'
+                        className='border border-black outline-none rounded px-5 py-2 mr-5' /> */}
+                    <input
+                        type="text"
+                        placeholder="Full Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="border border-black outline-none rounded px-5 py-2"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Phone number"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        className="border border-black outline-none rounded px-5 py-2"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="border border-gray-300 rounded px-4 py-2"
+                    />
+                    <input
+                        type="url"
+                        placeholder="Website URL"
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
+                        className="border border-gray-300 rounded px-4 py-2"
+                    />
+                </div>
+
                 <button className='bg-blue px-5 py-2 rounded text-white' onClick={generate}>Generate</button>
             </div>
 
@@ -35,10 +79,13 @@ function ContactUs() {
                     <div ref={qrRef} className='max-w-[500px] flex flex-col justify-between items-center bg-red-100 px-5 py-5'>
                         <p className='mb-10 font-bold text-2xl'>Scan QR Code</p>
                         <div className='border-2 border-black-200 p-5'>
-                            <QRCode
+                            <QRCodeSVG
                                 style={{ height: "auto", maxWidth: "200px", width: "200px", }}
+                                renderAs="canvas"
+                                size="400"
                                 value={data}
-                                bgColor="#fee2e2"
+                                title='Scan code'
+                                bgColor='#FEE2E2'
                             />
                         </div>
                     </div>
