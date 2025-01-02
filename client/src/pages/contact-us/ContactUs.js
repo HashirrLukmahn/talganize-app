387c7916e538699422cb90370a128b0ca47fae16
PraveenRevvster <praveen@baetho.com>
2025-01-02 18:56:45 +0530

    updated address on contact us page

--- client/src/pages/contact-us/ContactUs.js
+++ client/src/pages/contact-us/ContactUs.js
@@ -52,8 +52,8 @@ function ContactUs() {
                             </div>
                             <div>
                                 <h3 className="font-semibold">Our Location</h3>
-                                <p className="text-gray-600">99 S.t Jomblo Park Pekanbaru</p>
-                                <p className="text-gray-600">28292, Indonesia</p>
+                                <p className="text-gray-600">PLOT NO 216, PHASE 2, SAKET TOWNSHIP, SAINIKPURI,</p>
+                                <p className="text-gray-600">DR A S RAO NAGAR, SECUNDERABAD, HYDERABAD 500062</p>
                             </div>
                         </div>
 
@@ -64,7 +64,7 @@ function ContactUs() {
                             </div>
                             <div>
                                 <h3 className="font-semibold">Phone Number</h3>
-                                <p className="text-gray-600">(+62)81 414 257 9980</p>
+                                <p className="text-gray-600">9535806430</p>
                             </div>
                         </div>
 
@@ -75,49 +75,49 @@ function ContactUs() {
                             </div>
                             <div>
                                 <h3 className="font-semibold">Email Address</h3>
-                                <p className="text-gray-600">info@yourdomain.com</p>
+                                <p className="text-gray-600">info@talganize.com</p>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 {/* Right Column - Contact Form */}
-                <div className="flex-1 relative">
+                <div className="flex-1">
 
-                    <div className="bg-purple-700 p-8 rounded-lg max-w-[500px]">
-                        <h2 className='mb-10 text-white text-2xl font-semibold'>Send Message</h2>
+                    <div className="shadow border border-gray-light rounded p-8 max-w-[500px]">
+                        <h2 className='mb-10 text-black text-2xl font-semibold'>Send Message</h2>
                         <form className="flex flex-col space-y-4" ref={form} onSubmit={e => sendEmail(e)}>
                             <input
                                 type="text"
                                 placeholder="Your Name"
-                                className="w-full p-3 rounded-lg outline-none"
+                                className="w-full p-3 border outline-none border-gray-light rounded"
                                 name='from_name'
                                 required
                             />
                             <input
                                 type="email"
                                 placeholder="Your Email"
-                                className="w-full p-3 rounded-lg outline-none"
+                                className="w-full p-3 border outline-none border-gray-light rounded"
                                 name='from_email'
                                 required
                             />
                             <input
                                 type="tel"
                                 placeholder="Your Phone"
-                                className="w-full p-3 rounded-lg outline-none"
+                                className="w-full p-3 border outline-none border-gray-light rounded"
                                 name='phone_number'
                                 required
                             />
                             <textarea
                                 placeholder="Your Message"
                                 rows={4}
-                                className="w-full p-3 rounded-lg resize-none outline-none"
+                                className="w-full p-3 resize-none border outline-none border-gray-light rounded"
                                 name='message'
                                 required
                             />
                             <button
                                 type="submit"
-                                className="w-full bg-pink-500 text-white py-3 px-6 rounded-lg transition-colors"
+                                className="w-full bg-pink-500 text-white py-3 px-6 transition-colors"
                                 disabled={buttonDisable}
                             >
                                 {buttonDisable ? 'Please Wait..' : 'Submit'}
