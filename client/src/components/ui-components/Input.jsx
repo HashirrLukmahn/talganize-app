e763aae63aa164ac2b4718992a89f3f9c49a0cdb
PraveenRevvster <praveen@baetho.com>
2025-03-25 18:26:39 +0530

    created PostJob component

--- client/src/components/ui-components/Input.jsx
+++ client/src/components/ui-components/Input.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-const Input = ({ type = "text", placeholder, value, onChange, required = false, id }) => {
+const Input = ({ type = "text", name, placeholder, value, onChange, required = false, id }) => {
+
+
+    // className = "w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border border-gray-300 focus:border-secondary shadow-sm rounded-lg"
+
     return (
         <input
             {...(id && { id })}
             type={type}
+            name={name}
             required={required}
             placeholder={placeholder}
             value={value}
             onChange={onChange}
-            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border border-gray-300 focus:border-secondary shadow-sm rounded-lg"
+            className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-secondary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
         />
     );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,102 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        heading: '2.25rem',
+        subheading: '1.75rem',
+        body: '1rem',
+        small: '0.875rem'
+      },
+      colors: {
+        black: '000000',
+        white: '#FFFFFF',
+        'bg-gray': '#4B4847',
+        'gray-light': '#C9C5BA',
+        'gray-text': '#9BA9BD',
+        'talgan-green': '#155263',
+        'talgan-green-dark': '#124654',
+        'talgan-blue': '#6DD5FF',
+        yellow: '#FFBB09',
+        // primary: {
+        //   DEFAULT: 'hsl(var(--primary))',
+        //   foreground: 'hsl(var(--primary-foreground))'
+        // },
+        // secondary: {
+        //   DEFAULT: 'hsl(var(--secondary))',
+        //   foreground: 'hsl(var(--secondary-foreground))'
+        // },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        cta: '#FFBB09',
+        lightbg: '#F5F7F9',
+        border: 'hsl(var(--border))',
+        darktext: '#33302E',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))'
+        }
+      },
+      fontWeight: {
+        heading: '700',
+        subheading: '600',
+        body: '400',
+        small: '300'
+      },
+      borderRadius: {
+        btn: '0.5rem',
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      padding: {
+        'btn-y': '0.625rem',
+        'btn-x': '1.25rem'
+      }
+    },
+    fontFamily: {
+      sans: [
+        'Roboto',
+        'sans-serif'
+      ]
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -30,6 +124,7 @@ module.exports = {
         lightbg: '#F5F7F9',      // Lightest background (Job Card, Sections)
         border: '#E5E3DD',       // Borders, Muted text
         darktext: '#33302E',     // Dark contrast text
+        select: '#E5E7EB',         //Light Gray(Hover select option)
       },
       fontWeight: {
         heading: '700', // Bold for headings
@@ -44,12 +139,9 @@ module.exports = {
         'btn-y': '0.625rem', // 10px vertical padding
         'btn-x': '1.25rem', // 20px horizontal padding
       },
-      fontFamily: { sans: ['Roboto', 'sans-serif'], },
-      backgroundImage: {
-        'form-img': "url('src/assets/images/form-bg.png')",
-      },
-
     },
+    fontFamily: { sans: ['Roboto', 'sans-serif'], },
+
   },
   plugins: [],
 }
